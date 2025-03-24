87f731d49c8da7b6de0a8675636c400112819490
pratidnya11 <yallannavarpratidnya@gmail.com>
2025-03-24 10:56:21 +0530

    Add files via upload

---
+++ CharDemo.java
@@ -0,0 +1,12 @@
+public class CharDemo {
+	public static void main(String args[])
+	{
+	char myGrade='B';	
+	System.out.println(myGrade);
+	//ASCII values of A,B,C;
+	char myvar1=65, myvar2=66, myvar3=67;
+	System.out.println(myvar1);
+	System.out.println(myvar2);
+	System.out.println(myvar3);
+	}
+}
